ebf5959b5ad16047b37a5867e83ff1cf0099112e
Jolynn Collin <boryantheone228@yandex.ru>
2022-03-09 10:51:03 +0300

    Laba3 begin

---
+++ Laba3/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String in_filename;
+
+	    Scanner sc = new Scanner(System.in);
+	    int i = 0;
+        System.out.print("Vvedite chislo: ");
+        if (sc.hasNextInt()) {
+            i = sc.nextInt();
+        } else {
+            System.out.println("Sm.vyshe ^^^");
+        }
+        if (i == 0) {
+    }
+}
